17d1998fa1775f0dc48b6d4a23d373de592475dc
abdullahayazzz <162955635+abdullahayazzz@users.noreply.github.com>
2024-03-11 12:55:38 +0500

    Add files via upload

---
+++ main5.ts
@@ -0,0 +1,4 @@
+let famous_person:string = "David Goggins";
+let quote: string = "I don't stop when i'm tired, i stop when i'm done.";
+let message: string = `${famous_person} once said, "${quote}"`;
+console.log(message);
